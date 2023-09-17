881080996638c317ecdcd2f6d1155e66ee745745
Jacob Fashanu <fashanujacob@gmail.com>
2023-09-16 22:21:14 -0400

    format update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -26,7 +26,7 @@ export default function Search ({setStatsBackgroundColor, setTempColor, setLocat
         const response = await fetch(url);
         const data = await response.json();
         return data;
-      }
+    }
     
       
 
